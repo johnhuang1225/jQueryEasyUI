47da7f0c9e2b513d18688fa42bf32fd14001dbfb
johnhuang <johnhuang1225@gmail.com>
2015-01-21 18:25:41 +0800

    init lee jQueryEasyUI project

---
+++ lee/03/js/index.js
@@ -0,0 +1,36 @@
+$(function(){
+	
+	$('#box').draggable({
+		// revert:true,
+		// cursor:'text',
+		// handle:'#pox',
+		// edge:50,
+		// axis:'v',
+		// proxy:'clone',
+		// deltaX:10,
+		// deltaY:10,
+
+		// proxy:function(source){
+		// 	var p = $('<div style="width:400px;height:200px;border:1px dashed #ccc">');
+		// 	p.html($(source).html()).appendTo('body');
+		// 	return p;
+		// }
+		
+
+		// onBeforeDrag:function(e){
+		// 	alert('拖动之前觸發！');
+		// }
+
+
+	});
+
+
+
+	// $('#box').draggable('enable')
+
+	// console.log($('#box').draggable('options'));
+
+	
+
+
+});
