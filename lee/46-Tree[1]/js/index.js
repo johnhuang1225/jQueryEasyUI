06b82a1103fcd10dad29a20b8101e2f926cea465
johnhuang <johnhuang1225@gmail.com>
2015-01-23 17:22:19 +0800

    tree pracitce

--- lee/46-Tree[1]/js/index.js
+++ lee/46-Tree[1]/js/index.js
@@ -1,14 +1,22 @@
 $(function(){
 
 	$('#box').tree({
-		url:'tree.json',
+		url:'org.json',
 		animate:true,
 		checkbox:true,
 		lines:true,
-		formatter:function(node){
-			return '[' + node.text + ']';
-		}
+		// formatter:function(node){
+		// 	return '[' + node.text + ']';
+		// }
 	});
+
+	// use method
+	var data = [{"id":"1","parentid":"","text":"Apple"},{"id":"2","parentid":"1","text":"IT"},{"id":"3","parentid":"1","text":"HR"},{"id":"4","parentid":"2","text":"ap"},{"id":"5","parentid":"2","text":"info"}];	
+
+	$('#tree2').tree({
+		// line:true
+	});
+	$('#tree2').tree('loadData',data);
 
 
 });
